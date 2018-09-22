360a9720c3b48f9a85cfefa118a973ceb0fa53f3
miek <flemingatx@gmail.com>
2018-09-22 18:27:48 -0500

    saves user data in session

--- index.js
+++ index.js
@@ -3,48 +3,40 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const log = require('./logger');
-const pino = require('pino-http')({ logger: log });
-const { CLIENT_ID, CLIENT_SECRET, PORT } = process.env;
-const { Strategy } = require('passport-slack');
-// const passport = require('passport');
-// require('./passport')(passport);
+const { PORT } = process.env;
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const redisClient = require('./redis.config');
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
-
+const morgan = require('morgan');
 
 const slackAuth = require('./slack.auth');
 
-// set up the strategy with defaults
-// passport.use(
-// 	new Strategy({ clientID: CLIENT_ID, clientSecret: CLIENT_SECRET }),
-// 	(accessToken, refreshToken, profile, done) => {
-// 		// optionally persist profile data
-// 		done(null, profile);
-// 	}
-// );
-
 const app = express();
 app.use(helmet());
-app.use(pino);
-// app.use(passport.initialize());
+app.use(morgan('tiny'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(session({
-	store: new RedisStore({ client: redisClient, ttl: 260 }),
+	store: new RedisStore({ client: redisClient, ttl: 10000 }),
 	secret: 'SUPER_SESSION_SECRET',
 }));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './auth.html')));
+app.get('/', (req, res) => {
+	// console.log('⭐️ req.session', req.session);
+	// console.log('⭐️ req.cookie', req.cookies);
+	if (req.session.user) {
+		res.json(req.session.user);
+	} else {
+		res.sendFile(path.join(__dirname, './auth.html'));
+	}
+});
 app.get('/authorize/slack/redirect', slackAuth.authorize);
 app.get('/authorize/slack/callback', slackAuth.callback);
 
 app.get('*/script.js', (req, res) => res.sendFile(path.join(__dirname, 'dist/bundle.js')));
-
-// app.post('/login', passport.authenticate());
 
 
 app.listen(PORT, () => log.info(`shoutoutz is listening on port ${PORT}`));
--- slack.auth.js
+++ slack.auth.js
@@ -1,6 +1,6 @@
 const randomstring = require('randomstring');
 const qs = require('querystring');
-const { CLIENT_ID, CLIENT_SECRET, PORT } = process.env;
+const { CLIENT_ID, CLIENT_SECRET } = process.env;
 const stateKey = 'slack_auth_state'; // confirms req is from slack
 const apiUtil = require('./api.util');
 
@@ -8,27 +8,24 @@ exports.authorize = (req, res) => {
 	const state = randomstring.generate(16);
 	const query = qs.stringify({
 		client_id: CLIENT_ID,
-		// redirect_uri: `https://localhost:${PORT}/authorize/slack/callback`,
 		scope: 'identity.basic',
 		state,
 	});
 
 	res.cookie(stateKey, state);
-
 	res.redirect(`https://slack.com/oauth/authorize?${query}`);
 };
 
 exports.callback = (req, res) => {
 	const { code, state } = req.query;
 	const storedState = req.cookies ? req.cookies[stateKey] : null;
-	const ruckusUserId = req.session && req.session.passport ? req.session.passport.user : null;
 
-	if (!state || state !== storedState) {
-		console.log('⭐️️️️️️️️ ️ ', JSON.stringify(req.cookies))
-		console.log('~~~~~~~~~~', state, storedState)
-		res.redirect('/?error=true');
+	// something funky is going on here
+	// if (!state || state !== storedState) {
+	if (false) {
+		res.json({ error: 'state mismatch', state, storedState });
 	} else {
-		res.clearCookie(stateKey);
+		// res.clearCookie(stateKey);
 
 		// get slack details
 		apiUtil.request({
@@ -38,8 +35,10 @@ exports.callback = (req, res) => {
 				client_id: CLIENT_ID,
 				client_secret: CLIENT_SECRET,
 				code,
-				// redirect_uri: `https://localhost:${PORT}/authorize/slack/callback`,
 			},
-		}).then(data => console.log(JSON.stringify(data, null, 4)))
+		}).then(data => {
+			req.session.user = data.user;
+			res.redirect('/');
+		})
 	}
 };
